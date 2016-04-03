68c3c8c8e767877d68030c4cecc96d518ced13a3
Stadzior <Stadzior@gmail.com>
2016-04-04 00:14:22 +0200

    Added incomplete Solver implementation.

--- app/src/main/java/com/example/kamil/androidcalculator/Solver.java
+++ app/src/main/java/com/example/kamil/androidcalculator/Solver.java
@@ -1,12 +1,62 @@
 package com.example.kamil.androidcalculator;
+
+import java.util.Stack;
+
 /**
  * Created by Kamil on 2016-04-03.
  */
 public class Solver {
 
+    private static Stack<String> buildExpresionStack(String expression){
+        Stack<String> expressionStack = new Stack<String>();
+        return expressionStack;
+    }
+    private static Double resolveExpresionStack(Stack<String> expressionStack){
+        Double result = 0.0;
+        Double subResult = 0.0;
+        Integer initialStackSize = expressionStack.size();
+        for (int i = 0; i < initialStackSize; i++) {
+            result = solveExpression(expressionStack.pop(),subResult.toString());
+        }
+        return result;
+    }
+
+    private static Double solveExpression(String expression,String subResult){
+        Double evaluatedExpression = 0.0;
+        if(expression.contains("+")){
+
+        }else{
+            if(expression.contains("-")){
+
+            }else{
+                if(expression.contains("/")){
+
+                }else{
+                    if(expression.contains("*")){
+
+                    }else{
+                        if(expression.contains("^")){
+
+                        }else{
+                            if(expression.contains("√")){
+
+                            }else{
+                                if(expression.contains("log")){
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return evaluatedExpression;
+    }
+
     public static Double solveEquation(String expression){
-       // Expression e = new Expression(expression);
-        return 0.0;// e.calculate();
+        Stack<String> expressionStack = buildExpresionStack(expression);
+        Double result = resolveExpresionStack(expressionStack);
+        return result;
     }
 
 }
