4bb340955c49f338440dd6c0dac04de3d1b6a986
Stadzior <Stadzior@gmail.com>
2016-04-03 21:34:45 +0200

    Further layout adjustments

--- app/src/main/java/com/example/kamil/androidcalculator/MainActivity.java
+++ app/src/main/java/com/example/kamil/androidcalculator/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,9 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         setDisplayer((TextView) findViewById(R.id.textViewDisplayer));
+        displayer.setMovementMethod(new ScrollingMovementMethod());
     }
 
     @Override
