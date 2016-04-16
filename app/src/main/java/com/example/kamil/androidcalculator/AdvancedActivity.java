046a75e32cbd4f364139bd8a33b50616df8650a8
Stadzior <Stadzior@gmail.com>
2016-04-16 13:44:44 +0200

    Name refactoring.

--- app/src/main/java/com/example/kamil/androidcalculator/AdvancedActivity.java
+++ app/src/main/java/com/example/kamil/androidcalculator/AdvancedActivity.java
@@ -1,25 +1,20 @@
 package com.example.kamil.androidcalculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class AdvancedActivity extends AppCompatActivity {
 
     private TextView displayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_advanced);
         setDisplayer((TextView) findViewById(R.id.textViewDisplayer));
         displayer.setMovementMethod(new ScrollingMovementMethod());
     }
